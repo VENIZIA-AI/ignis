e21d55cc03a9746b6c182a8ef1606c30e1403192
TuanAnh <tuananh45468@gmail.com>
2025-12-02 16:27:44 +0700

    chore: remove unused rpc utility

--- packages/core/src/utilities/rpc.utility.ts
+++
@@ -1,4 +0,0 @@
-import type { BaseApplication } from '@/base/applications';
-
-export type AppType<T extends BaseApplication> = ReturnType<T['getRootRouter']>;
-export type InferAppType<T extends BaseApplication> = ReturnType<T['getRootRouter']>;
