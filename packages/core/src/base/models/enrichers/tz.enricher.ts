fa477d085a145cccd64abc8cbe97e3e6f1801c05
Hai Cao <haicao2805@gmail.com>
2025-12-10 20:59:23 +0700

    add [core]: deletedAt colume in generateTzColumnDefs

--- packages/core/src/base/models/enrichers/tz.enricher.ts
+++ packages/core/src/base/models/enrichers/tz.enricher.ts
@@ -5,21 +5,24 @@ import { TColumnDefinitions } from '../common/types';
 export type TTzEnricherOptions = {
   created?: { columnName: string; withTimezone: boolean };
   modified?: { enable: false } | { enable?: true; columnName: string; withTimezone: boolean };
+  deleted?: { enable: false } | { enable?: true; columnName: string; withTimezone: boolean };
 };
 
 export type TTzEnricherResult<ColumnDefinitions extends TColumnDefinitions = TColumnDefinitions> =
   ColumnDefinitions & {
     createdAt: NotNull<HasDefault<PgTimestampBuilderInitial<string>>>;
     modifiedAt?: NotNull<HasDefault<PgTimestampBuilderInitial<string>>>;
+    deletedAt?: NotNull<HasDefault<PgTimestampBuilderInitial<string>>>;
   };
 
 export const generateTzColumnDefs = (opts?: TTzEnricherOptions) => {
   const {
     created = { columnName: 'created_at', withTimezone: true },
     modified = { enable: true, columnName: 'modified_at', withTimezone: true },
+    deleted = { enable: true, columnName: 'deleted_at', withTimezone: true },
   } = opts ?? {};
 
-  const rs = {
+  let rs = {
     createdAt: timestamp(created.columnName, {
       mode: 'date',
       withTimezone: created.withTimezone,
@@ -28,21 +31,34 @@ export const generateTzColumnDefs = (opts?: TTzEnricherOptions) => {
       .notNull(),
   };
 
-  if (!modified.enable) {
+  if (!modified.enable && !deleted.enable) {
     return rs;
   }
 
-  return Object.assign({}, rs, {
-    modifiedAt: timestamp(modified.columnName, {
-      mode: 'date',
-      withTimezone: modified.withTimezone,
-    })
-      .defaultNow()
-      .notNull()
-      .$onUpdate(() => {
-        return new Date();
+  if (modified.enable) {
+    rs = Object.assign({}, rs, {
+      modifiedAt: timestamp(modified.columnName, {
+        mode: 'date',
+        withTimezone: modified.withTimezone,
+      })
+        .defaultNow()
+        .notNull()
+        .$onUpdate(() => {
+          return new Date();
+        }),
+    });
+  }
+
+  if (deleted.enable) {
+    rs = Object.assign({}, rs, {
+      deletedAt: timestamp(deleted.columnName, {
+        mode: 'date',
+        withTimezone: deleted.withTimezone,
       }),
-  });
+    });
+  }
+
+  return rs;
 };
 
 export const enrichTz = (baseSchema: TColumnDefinitions, opts?: TTzEnricherOptions) => {
