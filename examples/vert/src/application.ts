edfb8919c06fbc7dbc7c296f65ad93e9981c12f5
Hai Cao <haicao2805@gmail.com>
2025-12-16 16:42:07 +0700

    chore [vert]: uncomment postConfigure

--- examples/vert/src/application.ts
+++ examples/vert/src/application.ts
@@ -17,9 +17,13 @@ import {
   AuthenticationStrategyRegistry,
   BaseApplication,
   BaseMetaLinkModel,
+  BindingKeys,
+  BindingNamespaces,
+  DataTypes,
   DiskHelper,
   Environment,
   getError,
+  getUID,
   HealthCheckBindingKeys,
   HealthCheckComponent,
   HTTP,
@@ -247,134 +251,134 @@ export class Application extends BaseApplication {
   }
 
   async postConfigure(): Promise<void> {
-    // // ------------------------------------------------------------------------------------------------
-    // this.logger.info(
-    //   '[postConfigure] Inspect all of application binding keys: %s',
-    //   Array.from(this.bindings.keys()),
-    // );
-    //
-    // const configurationRepository = this.get<ConfigurationRepository>({
-    //   key: BindingKeys.build({
-    //     namespace: BindingNamespaces.REPOSITORY,
-    //     key: ConfigurationRepository.name,
-    //   }),
-    // });
-    //
-    // // ------------------------------------------------------------------------------------------------
-    // const case1 = await configurationRepository.findOne({
-    //   filter: { where: { code: 'CODE_1' } },
-    // });
-    // this.logger.info(
-    //   '[postConfigure] CASE_1 | Trying to findOne | condition: %j | rs: %o',
-    //   { filter: { where: { code: 'CODE_1' } } },
-    //   case1,
-    // );
-    //
-    // // ------------------------------------------------------------------------------------------------
-    // const case2 = await configurationRepository.find({
-    //   filter: {
-    //     where: { code: 'CODE_2' },
-    //     fields: { id: true, code: true, dataType: true, createdBy: true },
-    //     limit: 100,
-    //     include: [{ relation: 'creator' }],
-    //   },
-    // });
-    // this.logger.info(
-    //   '[postConfigure] CASE_2 | Trying to find result | condition: %j | fields: %j | limit: %s | rs: %o',
-    //   { where: { code: 'CODE_2' } },
-    //   { fields: { id: true, code: true, dataType: true } },
-    //   100,
-    //   case2,
-    // );
-    //
-    // // ------------------------------------------------------------------------------------------------
-    // const case3Payload = {
-    //   code: `CODE_${getUID()}`,
-    //   group: 'SYSTEM',
-    //   dataType: DataTypes.NUMBER,
-    //   nValue: int((Math.random() * 100).toFixed(2)),
-    // };
-    // const case3 = await configurationRepository.create({ data: case3Payload });
-    // this.logger.info(
-    //   '[postConfigure] CASE_3 | Trying to create | payload: %j | rs: %o',
-    //   case3Payload,
-    //   case3,
-    // );
-    //
-    // // ------------------------------------------------------------------------------------------------
-    // const case4Payload = [
-    //   {
-    //     code: `CODE_${getUID()}`,
-    //     group: 'SYSTEM',
-    //     dataType: DataTypes.NUMBER,
-    //     nValue: int((Math.random() * 100).toFixed(2)),
-    //   },
-    //   {
-    //     code: `CODE_${getUID()}`,
-    //     group: 'SYSTEM',
-    //     dataType: DataTypes.JSON,
-    //     jValue: { value: int((Math.random() * 100).toFixed(2)) },
-    //   },
-    // ];
-    // const case4 = await configurationRepository.createAll({ data: case4Payload });
-    // this.logger.info(
-    //   '[postConfigure] CASE_4 | Trying to create | payload: %j | rs: %o',
-    //   case4Payload,
-    //   case4,
-    // );
-    //
-    // // ------------------------------------------------------------------------------------------------
-    // const case5Payload = {
-    //   id: '89f1dceb-cb4b-44a6-af03-ea3a2472096c',
-    //   data: { nValue: int((Math.random() * 100).toFixed(2)) },
-    // };
-    // const case5 = await configurationRepository.updateById(case5Payload);
-    // this.logger.info(
-    //   '[postConfigure] CASE_5 | Trying to update | payload: %j | rs: %o',
-    //   case5Payload,
-    //   case5,
-    // );
-    //
-    // // ------------------------------------------------------------------------------------------------
-    // const case6Payload = {
-    //   data: {
-    //     nValue: int((Math.random() * 100).toFixed(2)),
-    //   },
-    //   where: {
-    //     id: '89f1dceb-cb4b-44a6-af03-ea3a2472096c',
-    //   },
-    //   options: { shouldReturn: false },
-    // };
-    // const case6 = await configurationRepository.updateAll(case6Payload);
-    // this.logger.info(
-    //   '[postConfigure] CASE_6 | Trying to update | payload: %j | rs: %o',
-    //   case6Payload,
-    //   case6,
-    // );
-    //
-    // // ------------------------------------------------------------------------------------------------
-    // const case7Payload = {
-    //   id: case3.data!.id,
-    //   options: { shouldReturn: true },
-    // };
-    // const case7 = await configurationRepository.deleteById(case7Payload);
-    // this.logger.info(
-    //   '[postConfigure] CASE_7 | Trying to delete | payload: %j | rs: %o',
-    //   case7Payload,
-    //   case7,
-    // );
-    //
-    // const case8Payload = {
-    //   where: {
-    //     and: [{ dataType: DataTypes.NUMBER }, { dataType: DataTypes.JSON }],
-    //   },
-    //   options: { shouldReturn: true },
-    // };
-    // const case8 = await configurationRepository.deleteAll(case8Payload);
-    // this.logger.info(
-    //   '[postConfigure] CASE_8 | Trying to delete | payload: %j | rs: %o',
-    //   case8Payload,
-    //   case8,
-    // );
+    // ------------------------------------------------------------------------------------------------
+    this.logger.info(
+      '[postConfigure] Inspect all of application binding keys: %s',
+      Array.from(this.bindings.keys()),
+    );
+
+    const configurationRepository = this.get<ConfigurationRepository>({
+      key: BindingKeys.build({
+        namespace: BindingNamespaces.REPOSITORY,
+        key: ConfigurationRepository.name,
+      }),
+    });
+
+    // ------------------------------------------------------------------------------------------------
+    const case1 = await configurationRepository.findOne({
+      filter: { where: { code: 'CODE_1' } },
+    });
+    this.logger.info(
+      '[postConfigure] CASE_1 | Trying to findOne | condition: %j | rs: %o',
+      { filter: { where: { code: 'CODE_1' } } },
+      case1,
+    );
+
+    // ------------------------------------------------------------------------------------------------
+    const case2 = await configurationRepository.find({
+      filter: {
+        where: { code: 'CODE_2' },
+        fields: { id: true, code: true, dataType: true, createdBy: true },
+        limit: 100,
+        include: [{ relation: 'creator' }],
+      },
+    });
+    this.logger.info(
+      '[postConfigure] CASE_2 | Trying to find result | condition: %j | fields: %j | limit: %s | rs: %o',
+      { where: { code: 'CODE_2' } },
+      { fields: { id: true, code: true, dataType: true } },
+      100,
+      case2,
+    );
+
+    // ------------------------------------------------------------------------------------------------
+    const case3Payload = {
+      code: `CODE_${getUID()}`,
+      group: 'SYSTEM',
+      dataType: DataTypes.NUMBER,
+      nValue: int((Math.random() * 100).toFixed(2)),
+    };
+    const case3 = await configurationRepository.create({ data: case3Payload });
+    this.logger.info(
+      '[postConfigure] CASE_3 | Trying to create | payload: %j | rs: %o',
+      case3Payload,
+      case3,
+    );
+
+    // ------------------------------------------------------------------------------------------------
+    const case4Payload = [
+      {
+        code: `CODE_${getUID()}`,
+        group: 'SYSTEM',
+        dataType: DataTypes.NUMBER,
+        nValue: int((Math.random() * 100).toFixed(2)),
+      },
+      {
+        code: `CODE_${getUID()}`,
+        group: 'SYSTEM',
+        dataType: DataTypes.JSON,
+        jValue: { value: int((Math.random() * 100).toFixed(2)) },
+      },
+    ];
+    const case4 = await configurationRepository.createAll({ data: case4Payload });
+    this.logger.info(
+      '[postConfigure] CASE_4 | Trying to create | payload: %j | rs: %o',
+      case4Payload,
+      case4,
+    );
+
+    // ------------------------------------------------------------------------------------------------
+    const case5Payload = {
+      id: '89f1dceb-cb4b-44a6-af03-ea3a2472096c',
+      data: { nValue: int((Math.random() * 100).toFixed(2)) },
+    };
+    const case5 = await configurationRepository.updateById(case5Payload);
+    this.logger.info(
+      '[postConfigure] CASE_5 | Trying to update | payload: %j | rs: %o',
+      case5Payload,
+      case5,
+    );
+
+    // ------------------------------------------------------------------------------------------------
+    const case6Payload = {
+      data: {
+        nValue: int((Math.random() * 100).toFixed(2)),
+      },
+      where: {
+        id: '89f1dceb-cb4b-44a6-af03-ea3a2472096c',
+      },
+      options: { shouldReturn: false },
+    };
+    const case6 = await configurationRepository.updateAll(case6Payload);
+    this.logger.info(
+      '[postConfigure] CASE_6 | Trying to update | payload: %j | rs: %o',
+      case6Payload,
+      case6,
+    );
+
+    // ------------------------------------------------------------------------------------------------
+    const case7Payload = {
+      id: case3.data!.id,
+      options: { shouldReturn: true },
+    };
+    const case7 = await configurationRepository.deleteById(case7Payload);
+    this.logger.info(
+      '[postConfigure] CASE_7 | Trying to delete | payload: %j | rs: %o',
+      case7Payload,
+      case7,
+    );
+
+    const case8Payload = {
+      where: {
+        and: [{ dataType: DataTypes.NUMBER }, { dataType: DataTypes.JSON }],
+      },
+      options: { shouldReturn: true },
+    };
+    const case8 = await configurationRepository.deleteAll(case8Payload);
+    this.logger.info(
+      '[postConfigure] CASE_8 | Trying to delete | payload: %j | rs: %o',
+      case8Payload,
+      case8,
+    );
   }
 }
