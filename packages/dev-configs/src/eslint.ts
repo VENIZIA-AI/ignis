1b4e4f19a87c354929f3883a40ce4549fc396d6e
Phat Nguyen <tanphat199@gmail.com>
2025-12-22 12:44:09 +0700

    add switch/case curly bracket rule

--- packages/dev-configs/src/eslint.ts
+++ packages/dev-configs/src/eslint.ts
@@ -1,11 +1,19 @@
 import mtLinterConfs from "@minimaltech/eslint-node";
 import type { Linter } from "eslint";
+import unicorn from "eslint-plugin-unicorn";
 
 export const eslintConfigs: Linter.Config[] = [
   ...mtLinterConfs,
   {
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
+  {
+    plugins: { unicorn },
+    rules: {
+      curly: ["error", "all"],
+      "unicorn/switch-case-braces": ["error", "always"],
     },
   },
 ];
